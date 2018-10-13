7688d9eff4363d640bc013e3ad70cd705b89dde4
“txoyp <1401741458@qq.com>
2018-10-13 14:33:29 +0800

    10_13_php

---
+++ myPhp/my.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+    class Car{
+        var $color;
+        function Car($color = "green"){
+            $this -> $color = $color;
+        }
+        function what_color(){
+            return $this -> color;
+        }
+    }
+    $c = new Car('red');
+    echo $c -> what_color();
+?>
+</body>
+</html>
